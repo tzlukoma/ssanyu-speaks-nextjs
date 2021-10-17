d962df97fc5850e6371dcfd87bdf5c7d32526f59
Thomas Z Lukoma <tzlukoma@gmail.com>
2021-10-17 16:17:31 -0400

    source book and bio info from cms with html styling

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
-import { client, urlFor } from '../lib/sanity'
-import { useNextSanityImage } from 'next-sanity-image'
 import groq from 'groq'
+import { useNextSanityImage } from 'next-sanity-image'
+import BlockContent from '@sanity/block-content-to-react'
+import { serializers } from '../lib/tailwindSerializer'
+import { client } from '../lib/sanity'
 
 function AboutPage({ siteSettings }) {
     const authorImageProps = useNextSanityImage(
@@ -21,48 +23,11 @@ function AboutPage({ siteSettings }) {
             </div>
             <div className='space-y-3 text-xl'>
                 <h1 className='text-3xl mt-4'>My Bio</h1>
-                <p>
-                    Minister Sentheia McLeod gave her life to the Lord at an early age. A
-                    native of Canton, Ohio, she graduated high school and moved to New
-                    Jersey to pursue her college career. Since then, she has received her
-                    Bachelor of Arts and Master of Arts in Psychology/Early Childhood
-                    Education and Advanced Curriculum and teaching.
-                </p>
-                <p>
-                    While Sentheia has served as a teacher and lead educator for the past
-                    21 years in the Plainfield Public School district, she has also
-                    committed her life to service for God’s Kingdom here on earth. She is
-                    a member of Cathedral International (the Historic Second Baptist
-                    Church) of Perth Amboy, New Jersey. In 2001, she answered her call to
-                    dance ministry and currently serves as Director of Dance Ministry.
-                    Sentheia answered her call and was licensed to preach the Gospel in
-                    2013 by her pastor, Bishop Donald Hilliard, Jr. Soon after she
-                    graduated from Drew Theological Seminary in 2016 with a M.A in Worship
-                    and Liturgical Arts. Her seminary experience led her to serve as a
-                    dance liaison and a Peace Ambassador for various peace Initiative at
-                    the United Nations (Annual International Day of Peace, Women’s Rights,
-                    Gun Violence Awareness). She completed her Internship at the Church
-                    Center for the United Nations, where she gathered children and
-                    worshippers to share the gospel in through dance and spoken word.{' '}
-                </p>
-                <p>
-                    With a desire to answer the next call on her life, Sentheia graduated
-                    from EITI Dance Year 1 Course and received her Dance Minister’s
-                    License in 2016 and was ordained Pastor under Set Free Ministries in
-                    2018. She is a 2018 graduate of The Eagles Network (TEN) New Jersey
-                    and currently serves as the USA Director and a staff developer for the
-                    EITI. She is appointed ambassador to the nations of Suriname and
-                    England. Sentheia enjoys sharing the Word of God through dance around
-                    the world.{' '}
-                </p>
-                <p>
-                    Above all these earthly accomplishments, Min. Sentheia McLeod is a
-                    fervent and lifelong worshiper servant of God, who loves the Lord and
-                    the Lord’s people! She counts it an extreme honor to be called to
-                    serve in the Kingdom for such a time as this. Min. Sentheia McLeod’s
-                    life mission is supported by two of her favorite scriptures: Genesis
-                    50:20 and Isaiah 61.
-                </p>
+                <BlockContent
+                    className="prose prose-lg 2xl:prose-xl"
+                    blocks={siteSettings[0].authorBio}
+                    serializers={serializers}
+                />
             </div>
         </div>
     )
--- client/pages/book.tsx
+++ client/pages/book.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
-import { motion } from 'framer-motion'
-import { client, urlFor } from '../lib/sanity'
-import { useNextSanityImage } from 'next-sanity-image'
 import groq from 'groq'
+import { useNextSanityImage } from 'next-sanity-image'
+import { motion } from 'framer-motion'
+import BlockContent from '@sanity/block-content-to-react'
+import { serializers } from '../lib/tailwindSerializer'
+import { client } from '../lib/sanity'
 import CustomLink from '../components/CustomLink'
 
 function BookPage({ siteSettings }) {
@@ -32,15 +34,11 @@ function BookPage({ siteSettings }) {
             </div>
             <div className="space-y-3 text-xl">
                 <h1 className="text-3xl mt-4">About the Book</h1>
-                <p>
-                    Dear God, where are you? I prayed. I think you made a big mistake and put me in the wrong family!
-                </p>
-                <p>
-                    I never felt like I belonged, and I always wanted out. The things I dreamed of and aspired to be were never evident in the people assigned to care for me, the only people I knew and loved. This is my story. It’s a story of pain, neglect, abuse, rejection, and chaos, but it’s also a story of grace, faith, forgiveness, acceptance, restoration, and love.
-                </p>
-                <p>
-                    Journey with me as I share how desire changed my life and how I came to know that God never makes a mistake! “We know that in all things God works for the good of those who love him, who have been called according to his purpose” (Romans 8:28, ESV). Let your heart find joy as you peek into my life and learn that through it all, God was there! There is a celebration, and the celebration is in my dance! New Life, New Dance is a heartfelt and inspiring story of victory.
-                </p>
+                <BlockContent
+                    className="prose prose-lg 2xl:prose-xl"
+                    blocks={siteSettings[0].bookDescription}
+                    serializers={serializers}
+                />
             </div>
         </div>
     )
