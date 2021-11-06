cbea22c029a72e01f7eb6b1f65dd591d704b2534
Thomas Z Lukoma <tzlukoma@gmail.com>
2021-11-06 18:01:16 -0400

    feat(events): add past events section

--- client/pages/events.tsx
+++ client/pages/events.tsx
@@ -9,34 +9,42 @@ import { serializers } from '../lib/customSerializer'
 import { client } from '../lib/sanity'
 import EventCard from '../components/EventCard'
 
-function EventsPage({ events }) {
-    const sortedEvents = events.sort(function (a: any, b: any) {
+function EventsPage({ upcomingEvents, pastEvents }) {
+    const sortedUpcomingEvents = upcomingEvents?.sort(function (a: any, b: any) {
         const firstItem: any = new Date(a.date)
         const secondItem: any = new Date(b.date)
         return firstItem - secondItem;
     });
-    console.log(sortedEvents)
+    const sortedPastEvents = pastEvents?.sort(function (a: any, b: any) {
+        const firstItem: any = new Date(a.date)
+        const secondItem: any = new Date(b.date)
+        return firstItem - secondItem;
+    });
+    console.log(sortedUpcomingEvents)
+    console.log(sortedPastEvents)
     return (
         <div className='sm:w-2/3 lg:w-1/2 m-auto grid lg:grid-cols-2 grid-cols-1 grid-rows-1 gap-1 p-5 font-sans'>
-            <div className=''>
-                {/* <Image
-                    {...authorImageProps}
-                    sizes='(max-width: 300px) 100vw, 300px'
-                    layout='responsive'
-                    alt='book image'
-                /> */}
-            </div>
             <div className='space-y-3 text-xl'>
                 <h1 className='text-3xl mt-4'>My Upcoming Events</h1>
                 <div>
-                    {events?.length > 0 ? (
-                        events?.map((event: any) => (
+                    {sortedUpcomingEvents?.length > 0 ? (
+                        sortedUpcomingEvents?.map((event: any) => (
                             <EventCard key={event._id} event={event} emptyCard={false} />
                         ))
                     ) : (
                         <div className='event-wrapper'>
                             <div className="event-none">No upcoming events</div>
                         </div>
+                    )}
+                </div>
+            </div>
+            <div className='space-y-3 text-xl'>
+                <h1 className='text-3xl mt-4'>Past Events</h1>
+                <div>
+                    {sortedPastEvents?.length > 0 && (
+                        sortedPastEvents?.map((event: any) => (
+                            <EventCard key={event._id} event={event} emptyCard={false} />
+                        ))
                     )}
                 </div>
             </div>
@@ -47,13 +55,19 @@ function EventsPage({ events }) {
 export default EventsPage;
 
 EventsPage.getInitialProps = async ctx => {
-    const events = await client
-        .fetch(
-            groq`
-	*[_type=="event"] 
-	`
-        )
-        .catch(console.error)
-
-    return { events }
+    const [upcomingEvents, pastEvents] = await Promise.all([
+        client
+            .fetch(
+                groq`
+	*[_type =="event" && status == "scheduled"]`
+            )
+            .catch(console.error),
+        client
+            .fetch(
+                groq`
+	*[_type =="event" && status == "completed"]`
+            )
+            .catch(console.error),
+    ])
+    return { upcomingEvents, pastEvents }
 }
